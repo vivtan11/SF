e8b582bbfbf541f73f613b11894282dbede68871
vivtan11 <vivsterr@gmail.com>
2020-01-03 21:00:37 +0530

    Create abstract-generate.py

---
+++ Entities/abstract-generate.py
@@ -0,0 +1,13 @@
+from xml.dom import minidom
+
+# read each line, search for lines starting with <abstract> and whose text doesn't start with '|', as
+# these are not usable abstracts
+
+with open("wiki-abstract.xml", "r") as fin:
+    with open("wiki-abstracts-only.txt", "w") as fout:
+        for line in fin:
+            if (line.startswith("<abstract>")) and not(line.startswith("<abstract>|")):
+				        # parse using built-in parser to get text between tags
+                p = minidom.parseString(line)
+                fout.write(p.getElementsByTagName('abstract')[0].firstChild.data)
+                fout.write("\n")
